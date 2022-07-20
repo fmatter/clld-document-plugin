b07e62a8aea6cc9d498cde28d0a3ca83ba4eae0e
Florian Matter <florianmatter@gmail.com>
2022-07-20 07:21:07 -0700

    neater slides js

--- src/clld_document_plugin/static/clld-slides.js
+++ src/clld_document_plugin/static/clld-slides.js
@@ -1,36 +1,33 @@
-function nextSlide() {
-    var target = $('.slide.active').next('.slide');
+function changeSlide(direction="forward") {
+    var dict = {
+        "back": "first",
+        "forward": "last"
+    }
+    if (direction=="back") {
+        var target = $('.slide.active').prev('.slide');
+    } else {
+        var target = $('.slide.active').next('.slide');
+    }
     if (target.length == 0) {
-        target = $('.slide:first');
+        target = $('.slide:'+dict[direction]);
     }
     $('.slide.active').removeClass('active');
     target.addClass('active');
     $('html, body').scrollTop(target.offset().top - 40);
 }
 
-function previousSlide() {
-    var target = $('.slide.active').prev('.slide');
-    if (target.length == 0) {
-        target = $('.slide:last');
-    }
-    $('.slide.active').removeClass('active');
-    target.addClass('active');
-    $('html, body').scrollTop(target.offset().top -40);
-}
-
-
 window.addEventListener("keydown", (e) => {
     if (e.key === "ArrowLeft") {
-        previousSlide();
+        changeSlide("back");
     } else if (e.key === "ArrowRight") {
-        nextSlide();
+        changeSlide();
     }
 })
 
 $(".next").click(function() {
-    nextSlide();
+    changeSlide();
 });
 
 $(".previous").click(function() {
-    previousSlide();
+    changeSlide("back");
 });
